b8b208878cfcaa45dd487029c08b3fb55b843afc
venkatesh-049 <venkateshvalmiki864@gmail.com>
2026-07-28 13:37:34 +0530

    Initial commit

---
+++ Day1.py
@@ -0,0 +1,12 @@
+#print("hello codegnan")
+'''name='venkatesh'
+print(name)
+email_id = 'venkateshvalmiki864@gmail.com'
+print(email_id)'''
+name,location,age = 'codegnan','hyd',8
+print(name)
+print(location)
+print(age)
+
+
+
