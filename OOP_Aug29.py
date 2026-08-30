9955f591107ca3aa42bc0f61ff1a58f37ce395dd
venkatesh-049 <venkateshvalmiki864@gmail.com>
2026-08-30 18:33:49 +0000

    Modules

---
+++ OOP_Aug29.py
@@ -0,0 +1,10 @@
+def greetings():
+    return "welcome to codegnan"
+#print(greetings())
+
+details={'company':'codegnan','batch':'DA-23','place':'hyd'}
+
+if __name__ == "__main__":
+    print(greetings())
+print(__name__)
+greetings()
